665630d07a07f0668f81f18f7e27fe2e7f40786d
RAKSHITH S B <70064036+SB-Rakshith@users.noreply.github.com>
2024-01-03 21:35:21 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyC5HWHvk7Ba1bwMMFLXn2dRbvFB6H4Gpm0",
-    authDomain: "agritecx.firebaseapp.com",
-    projectId: "agritecx",
-    storageBucket: "agritecx.appspot.com",
-    messagingSenderId: "199815048001",
-    appId: "1:199815048001:web:75e11026a60e15f5a5cfc0",
-    measurementId: "G-6DCM8ZXJSE"
-};
+    apiKey: "AIzaSyC6WP8Q_AvAwkxsPfT-N-Y72Ku-tE1hNPc",
+    authDomain: "agritecx-1.firebaseapp.com",
+    projectId: "agritecx-1",
+    storageBucket: "agritecx-1.appspot.com",
+    messagingSenderId: "40220200327",
+    appId: "1:40220200327:web:b5fc2506f44cec69de5722",
+    measurementId: "G-W23KJ4ZJNQ"
+  };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
